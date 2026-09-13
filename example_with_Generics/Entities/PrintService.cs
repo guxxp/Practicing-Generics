a79f28b81cc0666d655ef259a0a7b26516343d52
Gustavo Ferrreira <gustavohenriquesud033@gmail.com>
2026-09-12 21:30:56 -0300

    feat: folders

--- example_with_Generics/Entities/PrintService.cs
+++ example_with_Generics/Entities/PrintService.cs
@@ -6,18 +6,18 @@ using System.Threading.Tasks;
 
 namespace example_with_Generics.Entities
 {
-    // Classe genérica que armazena até 10 elementos do tipo G.
+    // Classe genérica que armazena até 10 elementos do tipo T.
     // Uso típico: criar uma instância, adicionar elementos, imprimir ou obter o primeiro.
-    internal class PrintService<G>
+    internal class PrintService<T>
     {
         // Array interno com capacidade fixa de 10 elementos.
-        private G[] _value = new G[10];
+        private T[] _value = new T[10];
         // Contador de elementos atualmente armazenados no array.
         private int _count = 0;
 
         // Adiciona um valor ao serviço.
         // Lança InvalidOperationException se a capacidade máxima for atingida.
-        public void AddValue(G value)
+        public void AddValue(T value)
         {
             if (_count == 10) throw new InvalidOperationException("PrintService is full");
             _value[_count] = value;
@@ -26,7 +26,7 @@ namespace example_with_Generics.Entities
 
         // Retorna o primeiro elemento armazenado.
         // Lança InvalidOperationException se nenhum elemento estiver presente.
-        public G First()
+        public T First()
         {
             if (_count == 0) throw new InvalidOperationException("PrintService is empty");
             return _value[0];
--- example_with_Generics/Program.cs
+++ example_with_Generics/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using example_with_Generics.Entities;
 
 namespace example_with_Generics
 {
@@ -10,6 +11,23 @@ namespace example_with_Generics
     {
         static void Main(string[] args)
         {
+            PrintService <int> printService = new PrintService<int>();
+
+            Console.WriteLine("How many values?");
+            int n = int.Parse(Console.ReadLine());
+
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                printService.AddValue(x);
+            }
+
+            
+            printService.Print();
+            Console.WriteLine("First: " + printService.First());
+
+            Console.ReadLine();
         }
     }
 }
